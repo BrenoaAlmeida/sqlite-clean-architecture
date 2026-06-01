ca3eaeccdfad59e9a1a22e08d10f07e37b2a545b
Breno Almeida <balmeida@tecnologiaunica.com.br>
2026-06-01 16:04:47 -0300

    Implementação do Scalar

--- API/Program.cs
+++ API/Program.cs
@@ -3,6 +3,7 @@ using Application.Interfaces;
 using Domain;
 using Repository;
 using Repository.Interfaces;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,19 +11,29 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 builder.Services.AddModelConfiguration(builder.Configuration);
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<ICarroRepository, CarroRepository>();
 builder.Services.AddScoped<ICarroService, CarroService>();
-
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// ==========================================
+// 2. CONFIGURAR O PIPELINE DE REQUISIÇÕES (Middlewares)
+// ==========================================
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    // Gera o endpoint do arquivo JSON (ex: /openapi/v1.json)
+    app.MapOpenApi();        
+
+    app.MapScalarApiReference(options =>
+    {
+        options.WithTitle("API de teste SQLITE")
+        .WithTheme(ScalarTheme.Moon);
+    });
+
+
 }
 
 app.UseHttpsRedirection();
